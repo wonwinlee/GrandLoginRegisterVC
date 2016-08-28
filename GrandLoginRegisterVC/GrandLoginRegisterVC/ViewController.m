ef33e69249394f5624bc9e2668fd59798a653d75
wonwinlee <wonwinry@163.com>
2016-08-28 17:06:04 +0800

    初始化项目

---
+++ GrandLoginRegisterVC/GrandLoginRegisterVC/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GrandLoginRegisterVC
+//
+//  Created by hudongshili on 16/8/28.
+//  Copyright © 2016年 hudongshili. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
