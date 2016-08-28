70dba1a51691e17635c787b350aa92bce55fb443
wonwinlee <wonwinry@163.com>
2016-08-28 17:41:06 +0800

    完工

--- GrandLoginRegisterVC/GrandLoginRegisterVC/AppDelegate.h
+++ GrandLoginRegisterVC/GrandLoginRegisterVC/AppDelegate.h
@@ -2,8 +2,10 @@
 //  AppDelegate.h
 //  GrandLoginRegisterVC
 //
-//  Created by hudongshili on 16/8/28.
-//  Copyright © 2016年 hudongshili. All rights reserved.
+//  Created by W_Lee on 16/8/28.
+//  Copyright © 2016年 W_Lee. All rights reserved.
+//  我的简书地址:http://www.jianshu.com/users/9be6d38c4bc5/latest_articles
+
 //
 
 #import <UIKit/UIKit.h>
